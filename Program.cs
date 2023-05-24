3b1c0deb133fa6ce73c5e9a10aaab9eee808edf7
Надежда Стоянова <134551735+Nadezhda2703@users.noreply.github.com>
2023-05-25 00:04:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace _01._USD_to_BGN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            double dollars = double.Parse(Console.ReadLine());
+            double rate = 1.79549;
+            
+            //calculation
+                double leva = dollars * rate;
+            //output
+            Console.WriteLine(leva);
+        }
+    }
+}
